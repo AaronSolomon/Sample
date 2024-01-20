3bf4817657b7a08f82a114611ad9cfc755ebb2c0
Solomon <solomon@lilina.csie.ncnu.edu.tw>
2024-01-20 21:50:01 +0800

    Input parameter in URL

--- hello.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-app = Flask(__name__)	# double underline
-
-@app.route('/')
-def index():
-    return "Hello, Flask\n"
--- img.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    html = '<img src="https://uniform.wingzero.tw/assets/images/badge/tw-kaohsiung-kghs.jpg">\n'
-    html += "Hello, <font color=magenta>kghs</FONT>\n"
-    return html
