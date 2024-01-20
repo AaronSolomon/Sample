ad5af6d3c5e5caf675f982436e659d29e61f5f0e
Solomon <solomon@lilina.csie.ncnu.edu.tw>
2024-01-20 21:59:26 +0800

    BMI examples for GET and POST

---
+++ bmi1.py
@@ -0,0 +1,21 @@
+# GET method
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/form')
+def form():
+    html = """<form action='/bmi'>
+    <input type=text name=height><br>
+    <input type=text name=weight><br>
+    <input type=submit>"""
+    return html
+
+@app.route('/bmi')
+def bmi():
+    h = int( request.values['height'] )   # request.values is a dict
+    w = int( request.values['weight'] )
+    bmi = w / (h/100)**2
+    html = '''Height: {}<br>
+    Weight: {}<br>
+    BMI: {:5.2f}'''.format(h, w, bmi)
+    return html
